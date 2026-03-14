2333f5646376ffac263243b0b3b0565f5972810e
naneaghabekyan <naneaghabekyan201@gmail.com>
2026-03-14 15:45:33 +0400

    Add files via upload
    Առաջադրանք — BankAccount 
    Պահանջներ 
    ● _balance → private
    ● Public մեթոդներ
    ○ Deposit(amount)
    ○ Withdraw(amount)
    ○ Balance (read-only property)
    ● Թույլ չտալ բացասական balance
    Bonus
    ● Ավելացնել TransferTo(BankAccount other, amount)

---
+++ Bank.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace BankAccount
+{
+    internal class Bank
+    {
+        private double _balance;
+
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        public Bank(double amount=0)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("Invalid value for balance! Balance is set to 0.");
+                amount = 0;
+            }
+            _balance=amount;
+        }
+
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+                Console.WriteLine("Amount you want to add can't be negative.");
+            else
+            {
+                _balance += amount;
+                Console.WriteLine("{0} was successfully added to your balance.",amount);
+                ShowBalance();
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < 0)
+                Console.WriteLine("Amount you want to withdraw can't be negative.");
+            else if (_balance < amount)
+                Console.WriteLine("Can't withdraw {0} from your balance.", amount);
+            else
+            {
+                _balance -= amount;
+                Console.WriteLine("You withdraw {0} from your balance.",amount);
+            }
+            ShowBalance();
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine("Your current balance:{0}\n ",Balance);
+        }
+
+        public void TransferTo(Bank other, double amount)
+        {
+            if (amount < 0)
+                Console.WriteLine("Amount for transfer can't be negative.");
+            else if(_balance < amount)
+                Console.WriteLine("There is no enough money on your balance to make a transfer.");
+            else
+            {
+                _balance -= amount;
+                other.Deposit(amount);
+                Console.WriteLine("{0} was successfully transfered",amount);
+                ShowBalance();
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("First client");
+            Bank first = new Bank(15000);
+            first.Deposit(120000);
+            first.Withdraw(20000);
+
+            Console.WriteLine("Second client");
+            Bank second = new Bank();
+            second.Deposit(120);
+            second.Withdraw(20000);
+            second.TransferTo(first, 14200);
+            Console.WriteLine("First client's balance {0}.",first.Balance);
+
+            Console.WriteLine("Third client");
+            Bank third = new Bank(-12365);
+            third.Deposit(120);
+            third.Withdraw(-20000);
+
+        }
+    }
+}
